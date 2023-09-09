c906bc3698d02157360510c4c622e31466c26612
juliorkm <julioramakrsnamandoju@gmail.com>
2023-09-08 23:32:28 -0300

    Adds Overlay Rebuilding skill

---
+++ script/xenorrkskills/c360007515.lua
@@ -0,0 +1,91 @@
+--Overlay Rebuilding
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddSkillProcedure(c,1,false)
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetLabel(0)
+	e1:SetCondition(s.condition)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return not Duel.IsExistingMatchingCard(aux.NOT(Card.IsType),tp,LOCATION_EXTRA,0,1,nil,TYPE_XYZ)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if e:GetLabel()==0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_FREE_CHAIN)
+		e1:SetCountLimit(1)
+		e1:SetCondition(s.flipcon)
+		e1:SetOperation(s.flipop)
+		Duel.RegisterEffect(e1,tp)
+	end
+	e:SetLabel(1)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--opd check
+	if Duel.GetFlagEffect(ep,id)>0 then return end
+	--condition
+	local tid=Duel.GetTurnCount()
+	return aux.CanActivateSkill(tp)
+		and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)<=0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp,tid)
+		and Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0
+end
+function s.filter(c,e,tp)
+	return c:IsType(TYPE_XYZ) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and (Duel.GetTurnCount()~=c:GetTurnID() or c:IsReason(REASON_RETURN))
+		and Duel.IsExistingMatchingCard(Card.IsMonster,tp,LOCATION_GRAVE,0,1,nil)
+end
+function s.filter1(c,e,tp)
+	local rk=c:GetRank()
+	local pg=aux.GetMustBeMaterialGroup(tp,Group.FromCards(c),tp,nil,nil,REASON_XYZ)
+	return (#pg<=0 or (#pg==1 and pg:IsContains(c))) and c:IsFaceup() and (rk>0 or c:IsStatus(STATUS_NO_LEVEL)) and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,c,rk+1,c:GetRace(),pg)
+end
+function s.filter2(c,e,tp,mc,rk,rc,pg)
+	if c.rum_limit and not c.rum_limit(mc,e) then return false end
+	return mc:IsType(TYPE_XYZ,c,SUMMON_TYPE_XYZ,tp) and c:IsRank(rk) and c:IsRace(rc) and (c:IsSetCard(0x1048) or c:IsSetCard(0x1073))
+		and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0 and mc:IsCanBeXyzMaterial(c,tp) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--opd register
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	--special summon
+	local tid=Duel.GetTurnCount()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,tid)
+	local tc=g:GetFirst()
+	Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	--attach
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+	local tc2=Duel.SelectMatchingCard(tp,s.attfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_EXTRA,0,1,1,nil,e):GetFirst()
+	if tc then
+		Duel.Overlay(tc,tc2,true)
+	end
+	--rank-up
+	local rk=tc:GetRank()
+	local pg=aux.GetMustBeMaterialGroup(tp,Group.FromCards(tc),tp,nil,nil,REASON_XYZ)
+	if (#pg<=0 or (#pg==1 and pg:IsContains(tc))) and c:IsFaceup() and (rk>0 or tc:IsStatus(STATUS_NO_LEVEL)) and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,tc,rk+1,tc:GetRace(),pg) then
+		if Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc,tc:GetRank()+1,tc:GetRace(),pg)
+			local sc=g:GetFirst()
+			if sc then
+				sc:SetMaterial(tc)
+				Duel.Overlay(sc,tc)
+				Duel.SpecialSummon(sc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)
+				sc:CompleteProcedure()
+		end
+	end
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
+end
