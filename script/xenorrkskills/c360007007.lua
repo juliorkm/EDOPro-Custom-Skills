69a8c43f8909ff0bad7587d6eaef46cdaaeab7f3
juliorkm <julioramakrsnamandoju@gmail.com>
2023-10-30 20:54:39 -0300

    Updates and renames Ties skill

--- script/xenorrkskills/c360007007.lua
+++ script/xenorrkskills/c360007007.lua
@@ -1,18 +1,19 @@
---The Tie That Binds
+--The Tie of Unity
 local s,id=GetID()
 function s.initial_effect(c)
 	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--condition
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
 	return aux.CanActivateSkill(tp)
-	and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil)
+		and g:GetClassCount(Card.GetCode)>=2
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
 	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
-	local atk=Duel.GetMatchingGroupCount(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)*100
+	local atk=g:GetClassCount(Card.GetCode)*100
 	if #g==0 or atk==0 then return end
 	for tc in aux.Next(g) do
 		local e1=Effect.CreateEffect(e:GetHandler())
@@ -22,16 +23,5 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetValue(atk)
 		tc:RegisterEffect(e1)
 	end
-	--flip during End phase
-	local e2=Effect.CreateEffect(e:GetHandler())
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_PHASE+PHASE_END)
-	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-	e2:SetCountLimit(1)
-	e2:SetOperation(s.flip)
-	e2:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e2,tp)
-end
-function s.flip(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
 end
