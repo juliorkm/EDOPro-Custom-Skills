0a62b46469127e822adc57d23f11d2bc3a9a04d8
juliorkm <julioramakrsnamandoju@gmail.com>
2023-10-31 01:36:07 -0300

    Fixes Gaia Power Overload skill

--- script/xenorrkskills/c360003505.lua
+++ script/xenorrkskills/c360003505.lua
@@ -1,4 +1,4 @@
---Overload
+--Gaia Power Overload
 local s,id=GetID()
 function s.initial_effect(c)
 	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)
@@ -35,32 +35,44 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetRange(LOCATION_MZONE)
 		e1:SetDescription(aux.Stringid(id,0))
 		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e1)
+		if not tc:IsType(TYPE_EFFECT) then
+			--Becomes an Effect Monster if it wasn't already one
+			local e2=Effect.CreateEffect(e:GetHandler())
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_ADD_TYPE)
+			e2:SetValue(TYPE_EFFECT)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e2,true)
+		end
 	end
 	--gain ATK/DEF
 	if count>=3 then
-		local e2=Effect.CreateEffect(tc)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_UPDATE_ATTACK)
-		e2:SetRange(LOCATION_MZONE)
-		e2:SetValue(s.atkval)
-		e2:SetDescription(aux.Stringid(id,1))
-		e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
-		tc:RegisterEffect(e2)
-		local e3=e2:Clone()
-		e3:SetCode(EFFECT_UPDATE_DEFENSE)
+		local e3=Effect.CreateEffect(tc)
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_UPDATE_ATTACK)
+		e3:SetRange(LOCATION_MZONE)
+		e3:SetValue(s.atkval)
+		e3:SetDescription(aux.Stringid(id,1))
+		e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
+		e3:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e3)
+		local e4=e3:Clone()
+		e4:SetCode(EFFECT_UPDATE_DEFENSE)
+		tc:RegisterEffect(e4)
 	end
 	--attack twice
 	if count>=5 then
-		local e4=Effect.CreateEffect(tc)
-		e4:SetType(EFFECT_TYPE_SINGLE)
-		e4:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
-		e4:SetValue(1)
-		e4:SetRange(LOCATION_MZONE)
-		e4:SetDescription(aux.Stringid(id,2))
-		e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
-		tc:RegisterEffect(e4)
+		local e5=Effect.CreateEffect(tc)
+		e5:SetType(EFFECT_TYPE_SINGLE)
+		e5:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
+		e5:SetValue(1)
+		e5:SetRange(LOCATION_MZONE)
+		e5:SetDescription(aux.Stringid(id,2))
+		e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
+		e5:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e5)
 	end
 	--destroy during next end
 	tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,2)
